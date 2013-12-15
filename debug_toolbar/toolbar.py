ryneeverett/django-debug-toolbar

commit 810a2fbc5157f619eb3d74c73c882f059360506b

Use collections.OrderedDict if available.

debug_toolbar/toolbar.py
@@ -9,8 +9,11 @@ import uuid
 from django.conf.urls import patterns, url
 from django.core.exceptions import ImproperlyConfigured
 from django.template.loader import render_to_string
-from django.utils.datastructures import SortedDict
 from django.utils.importlib import import_module
+try:
+    from collections import OrderedDict
+except ImportError:
+    from django.utils.datastructures import SortedDict as OrderedDict
 
 from debug_toolbar import settings as dt_settings
 
@@ -20,7 +23,7 @@ class DebugToolbar(object):
     def __init__(self, request):
         self.request = request
         self.config = dt_settings.CONFIG.copy()
-        self._panels = SortedDict()
+        self._panels = OrderedDict()
         for panel_class in self.get_panel_classes():
             panel_instance = panel_class(self)
             self._panels[panel_instance.panel_id] = panel_instance
@@ -61,7 +64,7 @@ class DebugToolbar(object):
 
     # Handle storing toolbars in memory and fetching them later on
 
-    _store = SortedDict()
+    _store = OrderedDict()
 
     def should_render_panels(self):
         render_panels = self.config['RENDER_PANELS']

debug_toolbar/panels/cache.py
@@ -10,8 +10,11 @@ from django.core.cache import cache as original_cache, get_cache as original_get
 from django.core.cache.backends.base import BaseCache
 from django.dispatch import Signal
 from django.template import Node
-from django.utils.datastructures import SortedDict
 from django.utils.translation import ugettext_lazy as _, ungettext
+try:
+    from collections import OrderedDict
+except ImportError:
+    from django.utils.datastructures import SortedDict as OrderedDict
 
 from debug_toolbar.panels import Panel
 from debug_toolbar.utils import (tidy_stacktrace, render_stacktrace,
@@ -139,7 +142,7 @@ class CachePanel(Panel):
         self.hits = 0
         self.misses = 0
         self.calls = []
-        self.counts = SortedDict((
+        self.counts = OrderedDict((
             ('add', 0),
             ('get', 0),
             ('set', 0),

debug_toolbar/panels/settings.py
@@ -3,7 +3,10 @@ from __future__ import absolute_import, unicode_literals
 from django.conf import settings
 from django.views.debug import get_safe_settings
 from django.utils.translation import ugettext_lazy as _
-from django.utils.datastructures import SortedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    from django.utils.datastructures import SortedDict as OrderedDict
 
 from debug_toolbar.panels import Panel
 
@@ -21,5 +24,6 @@ class SettingsPanel(Panel):
 
     def process_response(self, request, response):
         self.record_stats({
-            'settings': SortedDict(sorted(get_safe_settings().items(), key=lambda s: s[0])),
+            'settings': OrderedDict(sorted(get_safe_settings().items(),
+                                           key=lambda s: s[0])),
         })

debug_toolbar/panels/staticfiles.py
@@ -12,8 +12,11 @@ from django.contrib.staticfiles import finders, storage
 from django.contrib.staticfiles.templatetags import staticfiles
 
 from django.utils.translation import ungettext, ugettext_lazy as _
-from django.utils.datastructures import SortedDict
 from django.utils.functional import LazyObject
+try:
+    from collections import OrderedDict
+except ImportError:
+    from django.utils.datastructures import SortedDict as OrderedDict
 
 from debug_toolbar import panels
 from debug_toolbar.utils import ThreadCollector
@@ -132,7 +135,7 @@ class StaticFilesPanel(panels.Panel):
         of relative and file system paths which that finder was able
         to find.
         """
-        finders_mapping = SortedDict()
+        finders_mapping = OrderedDict()
         for finder in finders.get_finders():
             for path, finder_storage in finder.list([]):
                 if getattr(finder_storage, 'prefix', None):

debug_toolbar/panels/versions.py
@@ -4,9 +4,12 @@ import sys
 
 import django
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
-from django.utils.datastructures import SortedDict
 from django.utils.importlib import import_module
+from django.utils.translation import ugettext_lazy as _
+try:
+    from collections import OrderedDict
+except ImportError:
+    from django.utils.datastructures import SortedDict as OrderedDict
 
 from debug_toolbar.panels import Panel
 
@@ -46,6 +49,6 @@ class VersionsPanel(Panel):
             versions = sorted(versions, key=lambda version: version[0])
 
         self.record_stats({
-            'versions': SortedDict(versions),
+            'versions': OrderedDict(versions),
             'paths': sys.path,
         })

tests/panels/test_staticfiles.py
@@ -3,8 +3,6 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
-from django.contrib.staticfiles.templatetags import staticfiles
-from django.template import Context, RequestContext, Template
 
 from ..base import BaseTestCase
 

commit 7176b801a1eec4b1917035062e488163788f2530

Fix #499 - Regression from 810a2fbc.

debug_toolbar/toolbar.py
@@ -89,9 +89,12 @@ class DebugToolbar(object):
         cls = type(self)
         cls._store[self.store_id] = self
         for _ in range(len(cls._store) - self.config['RESULTS_STORE_SIZE']):
-            # When we drop support for Python 2.6 and switch to
-            # collections.OrderedDict, use popitem(last=False).
-            del cls._store[cls._store.keyOrder[0]]
+            try:
+                # collections.OrderedDict
+                cls._store.popitem(last=False)
+            except TypeError:
+                # django.utils.datastructures.SortedDict
+                del cls._store[cls._store.keyOrder[0]]
 
     @classmethod
     def fetch(cls, store_id):